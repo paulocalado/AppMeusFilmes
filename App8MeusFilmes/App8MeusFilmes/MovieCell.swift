91c3e5b5e87e29717433b3e642cb86223959899c
Paulo Henrique Calado Aoun <paulo_calado@live.com>
2018-05-04 12:23:32 -0300

    customizando table view

---
+++ App8MeusFilmes/App8MeusFilmes/MovieCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieCell.swift
+//  App8MeusFilmes
+//
+//  Created by Paulo Calado on 04/05/18.
+//  Copyright © 2018 Codgin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MovieCell: UITableViewCell{
+    
+    @IBOutlet weak var movieImage: UIImageView!
+    @IBOutlet weak var movieTitle: UILabel!
+    @IBOutlet weak var movieDescription: UILabel!
+}
--- App8MeusFilmes/App8MeusFilmes/ViewController.swift
+++ App8MeusFilmes/App8MeusFilmes/ViewController.swift
@@ -14,8 +14,16 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        filmes.append(Filme(titulo:"filme1", descricao: "descricao1", imagem: #imageLiteral(resourceName: "filme1")))
-        filmes.append(Filme(titulo:"filme 2", descricao:"descricao 2", imagem: #imageLiteral(resourceName: "filme2")))
+        filmes.append(Filme(titulo:"007 Spectre", descricao: "descricao 007", imagem: #imageLiteral(resourceName: "filme1")))
+        filmes.append(Filme(titulo:"Star Wars - The Last Jedi", descricao:"descricao Star Wars", imagem: #imageLiteral(resourceName: "filme2")))
+        filmes.append(Filme(titulo:"Impacto Mortal", descricao:"provavel lixo", imagem: #imageLiteral(resourceName: "filme3")))
+        filmes.append(Filme(titulo: "Deadpool", descricao: "deadpool", imagem:#imageLiteral(resourceName: "filme4")))
+        filmes.append(Filme(titulo: "O Regresso", descricao:"dafuck", imagem:#imageLiteral(resourceName: "filme5")))
+        filmes.append(Filme(titulo: "A Herdeira", descricao:"outra descricao",imagem:#imageLiteral(resourceName: "filme6")))
+        filmes.append(Filme(titulo: "Caçadores de emoção", descricao:"outra ruim",imagem:#imageLiteral(resourceName: "filme7")))
+        filmes.append(Filme(titulo: "Regresso do mal", descricao:"outra podre",imagem:#imageLiteral(resourceName: "filme8")))
+        filmes.append(Filme(titulo: "Tarzan", descricao:"oselva",imagem:#imageLiteral(resourceName: "filme9")))
+        filmes.append(Filme(titulo: "Hardcore", descricao:"outra descricao",imagem:#imageLiteral(resourceName: "filme10")))
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,9 +43,13 @@ class ViewController: UITableViewController {
         let filme:Filme = filmes[indexPath.row]
         let movieCell:String = "movieCell"
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: movieCell, for: indexPath)
-        cell.textLabel?.text = filme.titulo
-        cell.imageView?.image = filme.imagem
+        let cell = tableView.dequeueReusableCell(withIdentifier: movieCell, for: indexPath) as! MovieCell //Customização da célula
+        cell.movieTitle.text = filme.titulo
+        cell.movieDescription.text = filme.descricao
+        cell.movieImage.image = filme.imagem
+        /*cell.textLabel?.text = filme.titulo
+        cell.imageView?.image = filme.imagem*/
+        
         return cell
     }
 
